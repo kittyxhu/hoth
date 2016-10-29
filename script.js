e19934f9256969cf9b570e17a1104df0a60816fb
izzueZ <yizhu621@g.ucla.edu>
2016-10-29 13:12:35 -0700

    Basic

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-//Destin's comment
-//hello
-// edit
+var category = document.getElementById("category")
+var price = document.getElementById("price")
+var location = document.getElementById("location")
+var distance = document.getElementById("distance")
+
+console.log(category)

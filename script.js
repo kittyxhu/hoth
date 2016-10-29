ea669cf82b987bcfa3eb15014ec3e8ec9a79034b
adrayv <draym001@g.ucla.edu>
2016-10-29 16:08:13 -0700

    express happened

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var Yelp = require('yelp');
 
 var yelp = new Yelp({
-	consumer_key: fmO_UOsI15WIVfjr40yrkw,
-	consumer_secret: BTC23g-ZeHhCqS30-OLYPIabuL4,
-	token: 5RS9friuyIAXLdvv_uZS70XRndvvMCeb,
-	token_secret: gsAqtwYbsvkcSoSf2pb63mRCZQQ,
+	consumer_key: 'fmO_UOsI15WIVfjr40yrkw',
+	consumer_secret: 'BTC23g-ZeHhCqS30-OLYPIabuL4',
+	token: '5RS9friuyIAXLdvv_uZS70XRndvvMCeb',
+	token_secret: 'gsAqtwYbsvkcSoSf2pb63mRCZQQ',
 });
 
-yelp.search({ term: 'food', location: 'Montreal' })
+yelp.search({ term: 'food', location: 'Los Angeles', limit: 1 })
 .then(function (data) {
 	  console.log(data);
 })

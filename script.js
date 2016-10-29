ad53499992943eb12e2aff4f18dec5c63786fd95
adrayv <draym001@g.ucla.edu>
2016-10-29 13:10:31 -0700

    initial API

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-//Destin's comment
-//hello
-// edit
+var Yelp = require('yelp');
+
+var yelp = new Yelp({
+	consumer_key: fmO_UOsI15WIVfjr40yrkw,
+	consumer_secret: BTC23g-ZeHhCqS30-OLYPIabuL4,
+	token: 5RS9friuyIAXLdvv_uZS70XRndvvMCeb,
+	token_secret: gsAqtwYbsvkcSoSf2pb63mRCZQQ,
+});
+
+yelp.search({ term: 'food', location: 'Montreal' })
+.then(function (data) {
+	  console.log(data);
+})
+.catch(function (err) {
+	  console.error(err);
+});
+
